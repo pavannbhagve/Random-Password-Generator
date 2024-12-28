9e70bbc682e26e2439f3f1fe0834c2d869652e7a
PB <pb@gmail.com>
2024-12-28 14:19:11 +0530

    js done

--- mn.js
+++ mn.js
@@ -1,10 +1,42 @@
-//pop(),push(),shift(),unshift(),slice(),splice(),array.of().array.from().//
+const passwordBox = document.getElementById("Password");
+const length = 8;
 
-let y= [1,2,3,4];
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerCase = "abcdefghijklmnopqrstuvwxyz"
+const number = "0123456789"
+const symbol = "!@#$%^&*()_+-={}[]:<>?"
 
-y.pop();
+const allChars = upperCase + number + symbol + lowerCase;
 
-console.log(y);
+function creatPassword() {
 
+    let password ="";
+    password+= upperCase[Math.floor(Math.random()*upperCase.length)];
+    password+= lowerCase[Math.floor(Math.random()*lowerCase.length)];
+    password+= number[Math.floor(Math.random()*number.length)];
+    password+= symbol[Math.floor(Math.random()*symbol.length)];
 
+    while (password.length < length) {
 
+        password += allChars[Math.floor(Math.random()*allChars.length)];        
+    }
+
+    passwordBox.value = password;
+    
+    
+}
+
+function copyPassword() {
+    const password = passwordBox.value;
+    if (password) {
+        navigator.clipboard.writeText(password)
+            .then(() => {
+                alert("Password copied to clipboard!");
+            })
+            .catch(err => {
+                alert("Failed to copy password: " + err);
+            });
+    } else {
+        alert("No password to copy!");
+    }
+}
